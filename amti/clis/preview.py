543dfdbb6190ce72d8b4ba25e8751558fcc9091b
Jonathan Bragg <jbragg@allenai.org>
2020-04-11 16:26:12 -0700

    CLI option to run a local webserver to preview a batch of HITs

---
+++ amti/clis/preview.py
@@ -0,0 +1,100 @@
+"""CLI for running a simple web server to preview HIT"""
+
+import os
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+
+import click
+import jinja2
+
+from amti import actions
+from amti import settings
+from amti import utils
+
+
+class MyServer(HTTPServer):
+    def __init__(
+        self,
+        server_address,
+        RequestHandlerClass,
+        template_path=None,
+        data_path=None,
+    ):
+        super().__init__(server_address, RequestHandlerClass)
+        self.template_path = template_path
+        self.data_path = data_path
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self): # handle GET request
+        self._set_headers()
+
+        # Get data index from query (optional)
+        query = urlparse(self.path).query
+        try:
+            query_components = dict(qc.split("=") for qc in query.split("&"))
+        except ValueError:
+            query_components = {}
+        index = int(query_components.get('id', '0'))
+
+        # Render
+        with open(self.server.template_path, 'r') as template_file:
+            question_template = jinja2.Template(template_file.read())
+        with open(self.server.data_path, 'r') as data_file:
+            ln_data = {}
+            for i, f in enumerate(data_file):
+                if i == index:
+                    ln_data = json.loads(f)
+                    break
+        self.wfile.write(question_template.render(**ln_data).encode())
+
+def run(
+    template_path,
+    data_path,
+    port,
+    server_class=MyServer,
+    handler_class=MyHandler,
+):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class, template_path=template_path, data_path=data_path)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+
+@click.command(
+    context_settings={
+        'help_option_names': ['--help', '-h']
+    })
+@click.argument(
+    'definition_dir',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True))
+@click.argument(
+    'data_path',
+    type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.option(
+    '--port',
+    type=click.INT,
+    default=8000,
+    help='Choose a custom port'
+    )
+def preview_batch(definition_dir, data_path, port):
+    """Preview a batch of rendered HITs using DEFINITION_DIR and DATA_PATH.
+
+    Run a local web server to allow for previewing hits.
+    By default, runs at <http://localhost:8000/>.
+
+    You can preview row i by appending `?id=[i]`.
+    For example, for row 2, visit <http://localhost:8000/?id=2>.
+
+    """
+    run(
+        template_path=os.path.join(definition_dir, 'question.xml.j2'),
+        data_path=data_path,
+        port=port,
+    )
